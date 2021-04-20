bfebf84d1e4053a64b0993952c4b93929f08f7cb
David243 <Davidwong11219@gmail.com>
2021-04-20 15:04:19 -0400

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -2,30 +2,53 @@ package game;
 
 public class game{
 	
-	public static class Node{
-	int key;
-	Node left, right;
-		public Node(int x) { key = x; left = right = null;}
+	public static class Node
+	{
+		int key;
+		String story;
+		Node left, right;
+		public Node(int x, String story) 
+		{
+			key = x; 
+			this.story = story;
+			left = right = null;
+		}
 	}
 	
-	public static class binaryTree implements Runnable{
+	public static class binaryTree implements Runnable
+	{
 		Node root;
-		public binaryTree(int x){root = new Node(x);}
-		public binaryTree(){root = null;}
 		
-		public void add(int value) {
-		    root = addRecursive(root, value);
+		public binaryTree(int x, String story)
+		{
+			root = new Node(x, story);
 		}
-		private Node addRecursive(Node current, int value) {
-		    if (current == null) {
-		        return new Node(value);
+		public binaryTree()
+		{
+			root = null;
+		}
+		
+		public void add(int value, String story) 
+		{
+		    root = addRecursive(root, value, story);
+		}
+		private Node addRecursive(Node current, int value, String story) 
+		{
+		    if (current == null) 
+		    {
+		        return new Node(value, story);
 		    }
 
-		    if (value < current.key) {
-		        current.left = addRecursive(current.left, value);
-		    } else if (value > current.key) {
-		        current.right = addRecursive(current.right, value);
-		    } else {
+		    if (value < current.key) 
+		    {
+		        current.left = addRecursive(current.left, value, story);
+		    } 
+		    else if (value > current.key) 
+		    {
+		        current.right = addRecursive(current.right, value, story);
+		    } 
+		    else 
+		    {
 		        // value already exists
 		        return current;
 		    }
@@ -33,23 +56,28 @@ public class game{
 		    return current;
 		}
 		
-		public void traversal() {
-			if(root==null) {
-				System.out.println("error");
-			}else{
+		public void traversal() 
+		{
+			if(root!=null) 
+			{
 				traverseRecursive(root);
 				System.out.println();
 			}
 		}
 		
-		private void traverseRecursive(Node current) {
+		private void traverseRecursive(Node current) 
+		{
 			Node next;
-			if(current.left != null) {
+			if(current.left != null) 
+			{
 				next = current.left;
 				traverseRecursive(next);
 			}
-			System.out.print(current.key+" ");
-			if(current.right != null) {
+			
+			System.out.print(current.key + current.story + " ");
+			
+			if(current.right != null) 
+			{
 				next = current.right;
 				traverseRecursive(next);
 			}
@@ -66,19 +94,19 @@ public class game{
 	public static void main(String[] args) {
 		binaryTree bt = new binaryTree();
 		bt.traversal();
-	    bt.add(6);
+	    bt.add(6, "B");
 	    bt.traversal();
-	    bt.add(10);
+	    bt.add(10, "D");
 	    bt.traversal();
-	    bt.add(12);
+	    bt.add(12, "E");
 	    bt.traversal();
-	    bt.add(3);
+	    bt.add(3, "A");
 	    bt.traversal();
-	    bt.add(57);
+	    bt.add(57, "G");
 	    bt.traversal();
-	    bt.add(17);
+	    bt.add(17, "F");
 	    bt.traversal();
-	    bt.add(9);
+	    bt.add(9, "C");
 	    bt.traversal();
 	    
 	}
