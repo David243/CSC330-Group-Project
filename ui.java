538fdfc08a60752ec346a6aae280957025a0e2d5
ShadyKhaos <76458115+ShadyKhaos@users.noreply.github.com>
2021-04-27 15:41:27 -0400

    Update ui.java

--- ui.java
+++ ui.java
@@ -8,40 +8,84 @@ package game;
 	import javax.swing.JPanel;
 	
 public class ui {
-	public void displayScreen() {
-			
-			JFrame screen= new JFrame("Hitamo"); //frame
-	       
-			JPanel upper =new JPanel(); //upper
-		    upper.setBounds(0,0,1000,300);  //left cushion,top cushion, width, height  
-		    upper.setBackground(Color.white);//end of upper
-	       
-			
-			
-	        JPanel lower = new JPanel(); //lower
-
-	        lower.setBounds(0,300,1000,200);
-	        lower.setBackground(Color.black); 
-	       
-	        JButton left = new JButton("left");
-	        left.setBounds(100,100,300,300);
-	        left.setBackground(Color.white);
-	        
-	        
-	        JButton right = new JButton("right"); 
-	        right.setBackground(Color.red);
-	        
-	        
-	        lower.add(left);
-	        lower.add(right);
-	        
-	        screen.add(upper);  
-	        screen.add(lower);
-	        screen.setSize(1000,500);    
-	        screen.setLayout(null);    
-	        screen.setVisible(true); 
-	        
-		}
+	public void startScreen()
+	{
+		
+		Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+		Font sButton = new Font("Times New Roman", Font.PLAIN, 75);
+		
+		screen = new JFrame("Hitamo");
+		upper = new JPanel();
+		lower = new JPanel();
+		titleLabel = new JLabel("Hitamo");
+		start = new JButton("START");
+		
+		//setting window size
+		screen.setSize(1000,500);
+		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		screen.getContentPane().setBackground(Color.black);
+		screen.setLayout(null);
+		
+		upper.setBounds(0,0,1000,300); //left cushion,top cushion, width, height  
+		upper.setBackground(Color.black);
+		titleLabel.setForeground(Color.white);
+		titleLabel.setFont(titleFont);
+		
+		lower.setBounds(0,300,1000,200);
+		lower.setBackground(Color.black);
+		
+		start.setBackground(Color.blue);
+		start.setForeground(Color.white);
+		start.addActionListener(this);
+		start.setFont(sButton);
+		
+		upper.add(titleLabel);
+		lower.add(start);
+		screen.add(upper);
+		screen.add(lower);
+		screen.setVisible(true);
+	}
+	
+	public void gameScreen(String story, String lCh, String rCh) //(String story, String lCh, String rCh)
+	{	
+		Font sFont = new Font("Times New Roman", Font.PLAIN, 30);
+		Font cFont = new Font("Times New Roman", Font.PLAIN, 30);
+		
+        label1 = new JTextArea(story);
+        label1.setLineWrap(true);
+        label1.setWrapStyleWord(true);
+        label1.setBounds(0,10,900,300);
+        label1.setFont(sFont);
+		
+		upper = new JPanel(); //upper
+		upper.setBounds(0,0,1000,300);
+	    upper.setBackground(Color.white);//end of upper
+		
+        lower = new JPanel(); //lower
+        lower.setBounds(0,300,1000,200);
+        lower.setBackground(Color.black); 
+       
+        left = new JButton("test"); 
+        left.setBounds(100,100,300,300);
+        left.setBackground(Color.white);
+        left.addActionListener(this);
+        left.setFont(cFont);
+        
+        right = new JButton(rCh); 
+        right.setBackground(Color.red);
+        right.addActionListener(this);
+        right.setFont(cFont);
+        
+        upper.add(label1);
+        
+        lower.add(left);
+        lower.add(right);
+        
+        screen.add(upper);  
+        screen.add(lower);    
+        screen.setVisible(true); 
+        
+	}
 		
 		
 		
