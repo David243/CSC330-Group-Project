c30ae78c93ebbedfdd1841c6189c02596123d002
David243 <Davidwong11219@gmail.com>
2021-05-01 18:10:45 -0400

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,65 +1,67 @@
-package game;
+package Project;
 
 public class BinaryTree
+{
+	Node root;
+	
+	public BinaryTree(int x, Storage story)
 	{
-		Node root;
-		
-		public BinaryTree(int x, Storage story)
-		{
-			root = new Node(x, story);
-		}
-		public BinaryTree()
-		{
-			root = null;
-		}
-		
-		public void add(int value, Storage story) 
-		{
-		    root = addRecursive(root, value, story);
-		}
-		private Node addRecursive(Node current, int value, Storage story) 
-		{
-		    if (current == null) 
-		    {
-		        return new Node(value, story);
-		    }
-
-		    if (value < current.key) 
-		    {
-		        current.left = addRecursive(current.left, value, story);
-		    } 
-		    else if (value > current.key) 
-		    {
-		        current.right = addRecursive(current.right, value, story);
-		    } 
-		    else 
-		    {
-		        // value already exists
-		        return current;
-		    }
-
-		    return current;
-		}
+		root = new Node(x, story);
 	}
+	public BinaryTree()
+	{
+		root = null;
+	}
+	
+	public void add(int value, Storage story) 
+	{
+	    root = addRecursive(root, value, story);
+	}
+	private Node addRecursive(Node current, int value, Storage story) 
+	{
+	    if (current == null) 
+	    {
+	        return new Node(value, story);
+	    }
+
+	    if (value < current.key) 
+	    {
+	        current.left = addRecursive(current.left, value, story);
+	    } 
+	    else if (value > current.key) 
+	    {
+	        current.right = addRecursive(current.right, value, story);
+	    } 
+	    else 
+	    {
+	        // value already exists
+	        return current;
+	    }
+
+	    return current;
+	}
+}
 
 class Node
 {
-	int key;
-	String story;
-	String leLabel, rLabel;
-	Node left, right;
-	public Node(int x, Storage story) 
-	{
-		key = x;
-		this.story = story.story;
-		left = right = null;
-		leLabel = story.lText;
-		rLabel = story.rText;
-	}
-	public boolean isLeaf() {
-		if(this.left == null && this.right == null) 
-			return true;
-		else 
-			return false;
-	}
+int key;
+String story;
+String leLabel, rLabel;
+String type = null;
+Node left, right;
+public Node(int x, Storage story) 
+{
+	key = x;
+	this.story = story.story;
+	this.type = story.type;
+	left = right = null;
+	leLabel = story.lText;
+	rLabel = story.rText;
+}
+public boolean isLeaf() {
+	if(this.left == null && this.right == null) 
+		return true;
+	else 
+		return false;
+}
 }
