8b2b3dc7770f1875211992d0bf885ab6177d595b
MojeedB <65636486+MojeedB@users.noreply.github.com>
2021-05-01 15:23:55 -0400

    Update ui.java

--- ui.java
+++ ui.java
@@ -12,6 +12,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import java.util.ResourceBundle;
+import java.util.*;
+
+import edu.cuny.csi.csc330.project.GameChoices;
 
 public class UI implements ActionListener
 {
@@ -23,6 +26,7 @@ public class UI implements ActionListener
 	JTextArea label1;
 	JPanel lower;
 	JButton start, left, right, restart;
+	
 	
 	public void startScreen()
 	{
@@ -73,9 +77,9 @@ public class UI implements ActionListener
         label1.setBounds(0,10,900,300);
         label1.setFont(sFont);
 		
-	upper = new JPanel(); //upper
-	upper.setBounds(0,0,1000,300);
-	upper.setBackground(Color.white);//end of upper
+        upper = new JPanel(); //upper
+        upper.setBounds(0,0,1000,300);
+        upper.setBackground(Color.white);//end of upper
 		
         lower = new JPanel(); //lower
         lower.setBounds(0,300,1000,200);
@@ -122,7 +126,6 @@ public class UI implements ActionListener
         screen.add(lower); 
         screen.setVisible(true); 
 	}
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) 
@@ -179,64 +182,20 @@ public class UI implements ActionListener
 	}
 	
 	
-	private static Storage Node45 = new Storage();
-	private static Storage Node34 = new Storage();
-	private static Storage Node70 = new Storage();
-	private static Storage Node24 = new Storage();
-	private static Storage Node38 = new Storage();
-	private static Storage Node62 = new Storage();
-	private static Storage Node80 = new Storage();
-	private static Storage Node21 = new Storage();
-	private static Storage Node27 = new Storage();
-	private static Storage Node37 = new Storage();
-	private static Storage Node41 = new Storage();
-	private static Storage Node79 = new Storage();
-	private static Storage Node82 = new Storage();
-	private static Storage Node26 = new Storage();
-	private static Storage Node28 = new Storage();
-	private static Storage Node81 = new Storage();
-	private static Storage Node83 = new Storage();
-	
-	private static void initFromPropBundle() {
-		// so we're expecting a file in our CLASSPATH called
-		// PropertyBundleDemo.properties
-		ResourceBundle bundle = ResourceBundle.getBundle("GameChoices");
-		
-		String names[] = {"Node45","Node34","Node70","Node24","Node38","Node62", 
-	    		"Node80","Node21","Node27","Node37","Node41","Node79","Node82","Node26",
-	    		"Node28","Node81","Node83"};
-		Storage sArr[] = {Node45,Node34,Node70,Node24,Node38,Node62, 
-	    		Node80,Node21,Node27,Node37,Node41,Node79,Node82,Node26,
-	    		Node28,Node81,Node83};
-		
-        for(int i = 0;i<names.length;i++) {
-        if (bundle.containsKey(names[i])) {
-            sArr[i].story = bundle.getString(names[i]);
-        }
-        if (bundle.containsKey(names[i]+"L")) {
-            sArr[i].lText = bundle.getString(names[i]+"L");
-        }
-        if (bundle.containsKey(names[i]+"R")) {
-            sArr[i].rText = bundle.getString(names[i]+"R");
-        }
-        
-        }
-		
-	}
+
 	
 	
 	public static void main(String[] args) {
 
 	    UI s = new UI();
-	    initFromPropBundle();
+	    
+		GameChoices.initFromPropBundle();
+	    
 	    
 	    int arr[] = {45,34,70,24,38,62,80,21,27,37,41,79,82,26,28,81,83};
-	    Storage stArr[] = {Node45,Node34,Node70,Node24,Node38,Node62, 
-	    		Node80,Node21,Node27,Node37,Node41,Node79,Node82,Node26,
-	    		Node28,Node81,Node83};
-	    
-		for (int i = 0; i < stArr.length;i++) {
-			s.bt.add(arr[i],stArr[i]);
+	    Storage[] stringArr = Arrays.copyOf(GameChoices.stringArr(), arr.length);
+		for (int i = 0; i < arr.length;i++) {
+			s.bt.add(arr[i],stringArr[i]);
 		}
 	    
 //	    s.bt.add(45, Node45);
@@ -259,5 +218,6 @@ public class UI implements ActionListener
 	    s.startScreen();
 		
 	}
+
 
 }
